09abab19a4fc9b43d4f29bfd4fe1d7ad4012ab07
DDriggs00 <DDriggs00@gmail.com>
2019-09-04 19:08:35 -0700

    Completed tab.h file

--- vgo.tab.h
+++ vgo.tab.h
@@ -1,49 +1,61 @@
 /* adapted from the go 1.2.2 distribution */
 
-/* Tokens.  */
-#define LLITERAL 258    /* string literal */
-#define LINT 400    // Integer
-#define LDOUBLE 401 // Double
-#define LASOP 259	/* = */
-#define LCOLAS 260	/* := */
-#define LBREAK 261  // Linebreak?
-#define LCASE 262
-#define LCHAN 263
-#define LCONST 264
-#define LCONTINUE 265
-#define LDDD 266	/* ... */
-#define LDEFAULT 267
-#define LDEFER 268
-#define LELSE 269
-#define LFOR 271
-#define LFUNC 272
-#define LGO 273
-#define LGOTO 274
-#define LIF 275
-#define LIMPORT 276
-#define LINTERFACE 277
-#define LMAP 278
-#define LNAME 279
-#define LPACKAGE 280
-#define LRANGE 281
-#define LRETURN 282
-#define LSELECT 283
-#define LSTRUCT 284
-#define LTYPE 286
-#define LVAR 287
-#define LANDAND 288	/* && */
-#define LANDNOT 289	/* &^ */
-#define LBODY 290	/* { */
-#define LCOMM 291	/* <- */
-#define LDEC 292	/* -- */
-#define LEQ 293		/* == */
-#define LGE 294		/* >= */
-#define LGT 295		/* > */
-#define LIGNORE 296	/* */
-#define LINC 297	/* ++ */
-#define LLE 298		/* <= */
-#define LLSH 299	/* << */
-#define LLT 300		/* < */
-#define LNE 301		/* != */
-#define LOROR 302	/* || */
-#define LRSH 303	/* >> */
+/* Literals */
+#define LLITERAL        300 // String Literal
+#define LINT            301 // Integer
+#define LFLOAT          302 // Float
+#define LHEX            303 // Hexadecimal number
+#define LBIN            304 // Binary number
+#define LOCT            305 // Octal number
+#define LRUNE           306 // Rune (char)
+#define LVARNAME        307 // Variable name
+
+// Keywords
+#define LFUNC           400
+#define LMAP            401
+#define LSTRUC          402
+#define LELSE           403
+#define LPACKAGE        404
+#define LCONST          405
+#define LIF             406
+#define LTYPE           407
+#define LFOR            408
+#define LIMPORT         409
+#define LRETURN         410
+#define LVAR            411
+
+#define LBADKEYWORD     499
+
+// Variable types
+#define LTYPEBOOL       413
+#define LTYPESTRING     414
+#define LTYPEINT        415
+#define LTYPERUNE       416
+#define LTYPEFLOAT64    417
+
+#define LLE     500		/* <= */
+#define LGE     501		/* >= */
+#define LEQ     502		/* == */
+#define LNE     503		/* != */
+#define LANDAND 504	    /* && */
+#define LOROR   505	    /* || */
+#define LPLASN  506	    /* += */
+#define LMIASN  507	    /* -= */
+#define LPLPL   508	    /* ++ */
+#define LMIMI   509	    /* -- */
+#define LASN    510	    /* = */
+#define LLP     511	    /* ( */
+#define LRP     512	    /* ) */
+#define LLB     513	    /* [ */
+#define LRB     514		/* ] */
+#define LLT     515	    /* < */
+#define LRT     516	    /* > */
+#define LDOT    517	    /* . */
+#define LBANG   518		/* ! */
+#define LPLUS   519	    /* + */
+#define LMINUS  520	    /* - */
+#define LMUL    521	    /* * */
+#define LDIV    522	    /* / */
+#define LMOD    523	    /* % */
+
+#define LBADOPERATOR 599
