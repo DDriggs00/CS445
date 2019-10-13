579fb66d8e5d14df915a1ed9452ec08b2319955f
DDriggs00 <DDriggs00@gmail.com>
2019-10-13 12:35:50 -0700

    style improvement

--- include/token.h
+++ include/token.h
@@ -7,9 +7,9 @@ typedef struct token {
     int lineno;     /* the line number on which the token occurs */
     char *filename; /* the source file in which the token occurs */
     int ival;       /* for integer constants, store binary value here */
-    double dval;	   /* for real constants, store binary value here */
+    double dval;	/* for real constants, store binary value here */
     char *sval;     /* for string constants, malloc space, de-escape, store */
-                   /*    the string (less quotes and after escapes) here */
+                    /*    the string (less quotes and after escapes) here */
 } token_t;
 
 // Allocates and creates a new token
