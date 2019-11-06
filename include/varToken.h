699df7f1124dec24514da5bf6831105dcfb516a6
DDriggs00 <DDriggs00@gmail.com>
2019-11-05 22:33:26 -0800

    Changes to support type class

--- include/varToken.h
+++ include/varToken.h
@@ -4,18 +4,7 @@
 #include <stdbool.h>
 
 #include "cfuhash.h"
-
-#define NULL_TYPE    10000
-#define STRUCT_TYPE  10002
-#define ARRAY_TYPE   10003
-#define FUNC_TYPE    10005
-#define MAP_TYPE     10006
-#define MAIN_TYPE    10007
-#define INT_TYPE     10266
-#define STRING_TYPE  10267
-#define BOOL_TYPE    10268
-#define FLOAT64_TYPE 10269
-#define RUNE_TYPE    10270
+#include "type.h"
 
 typedef struct varToken_t {
     
@@ -28,6 +17,8 @@ typedef struct varToken_t {
     // For array and map
     int subType1, subType2;
     int arrSize;
+
+    type_t* type2;
 
     bool isConst;
     bool isInitialized;
@@ -44,6 +35,8 @@ varToken_t* varToken_create(char* scope, char* name, int type, int line);
 varToken_t* varToken_create_int(char* scope, char* name, int val, int line, bool isConst);
 varToken_t* varToken_create_str(char* scope, char* name, char* val, int line, bool isConst);
 varToken_t* varToken_create_float(char* scope, char* name, double val, int line, bool isConst);
+varToken_t* varToken_create_arr(char* scope, char* name, int type, int line);
+varToken_t* varToken_create_map(char* scope, char* name, int from, int to, int line);
 varToken_t* varToken_create_struct(char* scope, char* name, int line, cfuhash_table_t* ht);
 varToken_t* varToken_create_func(char* scope, char* name, int line, cfuhash_table_t* ht);
 
@@ -60,7 +53,6 @@ char* varToken_typeString(varToken_t* token);
 // Removes the token
 void varToken_remove();
 
-// Converts flex symbol to proper value
-int getProperTypeInt(int oldType);
+type_t* varToken_getType();
 
 #endif // VARTOKEN_H
