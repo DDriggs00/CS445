f0e9376855055101427d2dd01d478d5be8a8cfef
DDriggs00 <DDriggs00@gmail.com>
2019-11-10 12:39:53 -0800

    more tests

--- tests/symtabTests.go
+++ tests/symtabTests.go
@@ -1,31 +1,35 @@
 package main
 
 import "fmt"
-import "math/rand"
-import "time"
 
 var x int = 5
 var y float64 = 10.5
 var z bool = true
-var a []rune = 'a'
-var n [5]rune
+var a [3]rune
+
 const b string = "test"
+
 var m map[string]int
+var n map[string]bool
 
 type person struct {
-    name string
-    age  int
+	name string
+	age  int
 }
 
 func main() {
-    var q int
-    q = 5
-    z = false
+	var q int
+	q = 5
+	z = false
+	var n1 float64
+	fmt.Println(n1)
+	q = (4 + 5) - 10 + (2 + q)
+	if q == 5 {
+		q = 1
+	}
 
-    var n1 float64
-    fmt.Println(n1)
-    
 }
+
 func f2(x1, x2 float64, y1, y2 int) {
 	var y float64
 }
