0ca3f678b3ada06a8886b5d770ff3c42ca0a3db8
DDriggs00 <DDriggs00@gmail.com>
2019-11-14 22:22:25 -0800

    Removed useleess test cases

--- tests/art.go
+++
@@ -1,9 +0,0 @@
-package main
-import "fmt"
-func main() {
-	var x int
-	x = 15
-	fmt.Println("------------------------------------------")
-	fmt.Println("|  this |  is  |  ",  x,  "  | what | I | meant |" )
-	fmt.Println("------------------------------------------")
-}
